01ef291c3d37ce61a7117d4cbd4a679dd432e4f4
JAlexanderSN <2558467@cegepmontpetit.ca>
2026-08-28 14:57:55 -0400

    PROGRES CRÉATION DU CONTRÔLEUR ZOMBIETYPE
    Le contrôleur n'est pas fini, mais est en processus

---
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZombieParty.Models;
+
+namespace ZombieParty.Controllers
+{
+    public class ZombieTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ViewBag.MaListe = new List<ZombieType>()
+            {
+                    new ZombieType(){m_TypeName= "Virus", m_Id=1},
+                new ZombieType(){m_TypeName= "Contact", m_Id=2}
+            };
+
+            return View();
+        }
+    }
+}
