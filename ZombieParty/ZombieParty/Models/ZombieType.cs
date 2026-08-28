1cb7684b49afe2a172341f21a184d54794156247
JAlexanderSN <2558467@cegepmontpetit.ca>
2026-08-28 14:23:18 -0400

    FCT classe ZombieType
    Ajout de la classe ZombieType

---
+++ ZombieParty/ZombieParty/Models/ZombieType.cs
@@ -0,0 +1,33 @@
+﻿namespace ZombieParty.Models
+{
+    public class ZombieType
+    {
+        // Champs
+
+        public int m_Id;
+        
+        public string m_TypeName;
+
+        
+        // Propiétés
+
+        public int Id
+        {
+            get
+            {
+                return m_Id;
+            }
+        }
+
+        public string TypeName
+        {
+            get
+            {
+                return m_TypeName;
+            }
+        }
+
+        
+
+    }
+}
