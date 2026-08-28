f216750d275ac41016153d6eece303052c2f5f97
JAlexanderSN <2558467@cegepmontpetit.ca>
2026-08-28 10:32:29 -0400

    PROGRES AFFICHAGE DE LA LISTE DES ZOMBIES
    Le processus d'afficher une liste de zombies n'est pas fini, mais est en processus.

---
+++ ZombieParty/ZombieParty/Models/Zombie.cs
@@ -0,0 +1,31 @@
+﻿namespace ZombieParty.Models
+{
+    public class Zombie
+    {
+        // Champs 
+        
+        public string m_Name;
+
+        public string m_Type;
+
+        public int m_Point;
+
+
+        // Propiétés 
+
+        public string Name
+        {
+
+            get
+            {
+                return m_Name;
+            }
+
+        }
+
+
+
+
+
+    }
+}
